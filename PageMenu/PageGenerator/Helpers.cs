9f34b8e90bde582b1ee39498343a63bc7f3f1614
mperdeck <black@hole.com>
2020-10-17 22:16:40 +1100

    Generator that only generates paras and headers

---
+++ PageMenu/PageGenerator/Helpers.cs
@@ -0,0 +1,99 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PageGenerator
+{
+    public static class Helpers
+    {
+        static int WordIndex = 0;
+        static int CurrentIndent = 0;
+
+        public static void AddPageStart(this StringBuilder sb)
+        {
+            sb.AppendLine(@"<!DOCTYPE html>");
+            sb.AppendLine(@"<html>");
+            sb.AppendLine(@"<head><link rel=""stylesheet"" href=""style.css""></head>");
+            sb.AppendLine(@"<body>");
+            sb.AppendLine(@"<div class=""inner"">");
+        }
+
+        public static void AddPageEnd(this StringBuilder sb)
+        {
+            sb.AppendLine(@"</div>");
+            sb.AppendLine(@"<script src=""formmenu.js""></script>");
+            sb.AppendLine(@"</body>");
+            sb.AppendLine(@"</html>");
+        }
+
+        public static void AddHeading(this StringBuilder sb, int level)
+        {
+            // Indent of this heading will be that produced by the parent heading
+            CurrentIndent = (level - 1) * 4;
+
+            sb.AppendLine(@"");
+            string caption = GetRandomWord().Humanize(LetterCasing.Title);
+            string line = $"<H{level}>{caption}</H{level}>";
+            sb.AddIndentedLine(line);
+
+            CurrentIndent = level * 4;
+        }
+
+        public static void AddLoremIpsum(this StringBuilder sb, int numParagraphs)
+        {
+            LoremIpsum(sb, 5, 40, 1, 5, numParagraphs);
+        }
+
+        public static void AddIndentedLine(this StringBuilder sb, string line)
+        {
+            string indentedLine = (new String(' ', CurrentIndent)) + line;
+            sb.AppendLine(indentedLine);
+        }
+
+        public static void WriteToFile(this StringBuilder sb, string filePath)
+        {
+            string text = sb.ToString();
+            File.WriteAllText(filePath, text);
+        }
+
+        public static string GetRandomWord()
+        {
+            int nbrWords = EnglishWords.LongList.Count;
+            string word = EnglishWords.LongList[(WordIndex++) % nbrWords];
+            return word;
+        }
+
+        public static void LoremIpsum(StringBuilder sb, int minWords, int maxWords,
+                                            int minSentences, int maxSentences,
+                                            int numParagraphs)
+        {
+
+            var words = new[]{"lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
+        "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
+        "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"};
+
+            var rand = new Random();
+            int numSentences = rand.Next(maxSentences - minSentences)
+                + minSentences + 1;
+            int numWords = rand.Next(maxWords - minWords) + minWords + 1;
+
+            for (int p = 0; p < numParagraphs; p++)
+            {
+                sb.AddIndentedLine("<p>");
+                for (int s = 0; s < numSentences; s++)
+                {
+                    sb.Append(new String(' ', CurrentIndent + 4));
+                    for (int w = 0; w < numWords; w++)
+                    {
+                        if (w > 0) { sb.Append(" "); }
+                        sb.Append(words[rand.Next(words.Length)]);
+                    }
+                    sb.AppendLine(". ");
+                }
+                sb.AddIndentedLine("</p>");
+            }
+        }
+    }
+}
---
+++ PageMenu/PageGenerator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace PageGenerator
+{
+    // Writes test page to current directory
+
+
+    /// <summary>
+    /// Writes test page to file.
+    /// File path (relative to current dir) is given in first argument.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string filePath = args[0];
+            if (string.IsNullOrEmpty(filePath))
+            {
+                Console.WriteLine("No file path given.");
+            }
+
+            var sb = new StringBuilder();
+            sb.AddPageStart();
+            sb.AddHeading(1);
+
+            sb.AddLoremIpsum(1);
+
+            for(i = 0; i < 3; i++)
+            {
+                sb.AddHeading(2);
+                sb.AddLoremIpsum(4);
+            }
+
+            sb.AddHeading(2);
+
+            for (i = 0; i < 3; i++)
+            {
+                sb.AddHeading(3);
+                sb.AddLoremIpsum(4);
+            }
+
+            sb.AddHeading(2);
+            sb.AddLoremIpsum(4);
+
+            sb.AddPageEnd();
+            sb.WriteToFile(filePath);
+        }
+    }
+}
