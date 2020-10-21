054a644944ee98d4194d4c9a504976605f63574f
mperdeck <black@hole.com>
2020-10-21 20:29:18 +1100

    loading all header dom elements

--- PageMenu/PageMenu/formmenu.ts
+++ PageMenu/PageMenu/formmenu.ts
@@ -2,6 +2,9 @@
 // "load" fires when the entire page has loaded, including stylesheets, etc.
 
 document.addEventListener("DOMContentLoaded", function(){
+
+    FormMenu.pageLoadedHandler();
+
     console.log('page has loaded');
 
     var list = document.querySelector('#list')
@@ -21,3 +24,46 @@ document.addEventListener("DOMContentLoaded", function(){
 
 });
 
+
+
+
+namespace FormMenu {
+    class MenuElementInfo {
+        // The item in the menu
+        menuElement: HTMLElement;
+
+        // The heading, etc. in the actual DOM (not in the menu)
+        domElement: HTMLElement;
+
+        // Caption of the menu element
+        caption: string;
+    }
+
+    let menuElementInfos: MenuElementInfo[];
+
+    function getAllDomElements(): NodeListOf<Element> {
+        let allDomElements = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+        return allDomElements;
+    }
+
+    function domElementsToMenuElements(domElements: NodeListOf<Element>): MenuElementInfo[] {
+        return null;
+    }
+
+    function domElementToMenuElement(domElement: HTMLElement): HTMLElement {
+        return null;
+    }
+
+    function createMenu(menuElementInfos: MenuElementInfo[]): HTMLElement {
+        return null;
+    }
+
+    export function pageLoadedHandler(): void {
+            console.log(555);
+
+        let x = domElementsToMenuElements(getAllDomElements());
+
+
+    }
+
+}
