f250fe33086c053b8ccae36321574c5400b6067b
mperdeck <black@hole.com>
2020-10-31 13:43:25 +1100

    create html5 form config file, adding label to dom elements to put in menu

---
+++ PageMenu/PageMenu/html5form.formmenu.config.ts
@@ -0,0 +1,15 @@
+///<reference path="formmenu.d.ts" />
+
+// -----------------------------------
+// Always load the formmenu.js file before any ...formmenu.config.js files
+// -----------------------------------
+
+namespace FormMenu {
+    export var formMenuConfiguration: iFormMenuConfiguration;
+
+    formMenuConfiguration.querySelector = "h1,h2,h3,h4,h5,h6,label";
+
+
+}
+
+
