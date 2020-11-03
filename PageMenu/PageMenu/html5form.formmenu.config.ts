1fd14d1700abd43b32d738b1865ec1c0a66f5b0a
mperdeck <black@hole.com>
2020-11-03 23:10:22 +1100

    add html5invalid

--- PageMenu/PageMenu/html5form.formmenu.config.ts
+++ PageMenu/PageMenu/html5form.formmenu.config.ts
@@ -28,11 +28,34 @@ namespace FormMenu {
         (domElement: HTMLElement, setActive: (active: boolean)=>void)=> {
             if (domElement.tagName.toLowerCase() !== 'label') { return; }
 
+// DOES NOT WORK FOR select, textarea, etc.
+
+
             let labelElement: HTMLLabelElement = domElement as HTMLLabelElement;
             let inputElementId = labelElement.htmlFor;
             let inputElement: HTMLInputElement = document.getElementById(inputElementId) as HTMLInputElement;
             setActive(inputElement.required);
         });
-}
+
+        formMenuConfiguration.itemStateInfos["html5invalid"] = new Html5ItemStateInfo(
+            'formmenu-is-invalid', 'formmenu-is-parent-of-invalid', 
+            'formmenu-invalid-filter-is-active', 'formmenu-invalid-filter-button', 
+            (domElement: HTMLElement, setActive: (active: boolean)=>void)=> {
+                if (domElement.tagName.toLowerCase() !== 'label') { return; }
+    
+                let labelElement: HTMLLabelElement = domElement as HTMLLabelElement;
+                let inputElementId = labelElement.htmlFor;
+                let inputElement: HTMLInputElement = document.getElementById(inputElementId) as HTMLInputElement;
+
+
+                // DOES NOT WORK FOR select, textarea, etc.
+
+                setActive(!inputElement.validity.valid);
+
+                inputElement.addEventListener("input", function(){
+                    setActive(!inputElement.validity.valid);
+                });
+            });
+    }
 
 
