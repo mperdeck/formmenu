e2e8ef109dc1d24b918cc3e57040614c60b5d827
mperdeck <black@hole.com>
2020-11-28 11:02:19 +1100

    load bootstrap from local disk

--- PageMenu/PageGenerator/Helpers.cs
+++ PageMenu/PageGenerator/Helpers.cs
@@ -22,7 +22,7 @@ namespace PageGenerator
                 switch(externalLibrary.Value)
                 {
                     case ExternalLibrary.Bootstrap3:
-                        sb.AppendLine(@"<link rel=""stylesheet"" href=""https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"" integrity=""sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"" crossorigin=""anonymous"">");
+                        sb.AppendLine(@"<link rel=""stylesheet"" href=""bootstrap-3.3.7-dist/css/bootstrap.min.css"">");
                         break;
                 }
             }
@@ -62,7 +62,7 @@ namespace PageGenerator
                 switch (externalLibrary.Value)
                 {
                     case ExternalLibrary.Bootstrap3:
-                        sb.AppendLine(@"<script src=""https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"" integrity=""sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"" crossorigin=""anonymous""></script>");
+                        sb.AppendLine(@"<script src=""bootstrap-3.3.7-dist/js/bootstrap.min.js""></script>");
                         break;
                 }
             }
