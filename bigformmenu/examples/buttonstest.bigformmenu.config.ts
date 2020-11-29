674e2abbb6d2faabd6d32ed5dbbb4c1caefcc2b5
mperdeck <black@hole.com>
2020-11-29 15:04:04 +1100

    Create config without creating a class

--- bigformmenu/examples/buttonstest.bigformmenu.config.ts
+++ bigformmenu/examples/buttonstest.bigformmenu.config.ts
@@ -10,34 +10,27 @@ namespace BigFormMenu {
     // Will hold reference to the Save button generated below the form
     export var menuSaveButton: HTMLButtonElement;
 
-    class MenuButton implements  iMenuButton {
-        constructor(
-            public caption: string,
-            public onClick: ()=>void,
-            public cssClass?: string,
-            public wireUp?: (buttonElement: HTMLButtonElement)=>void
-        ) {}
-    }
-
     // if menuButtons is undefined, set it now
     if (!bigFormMenuConfiguration.menuButtons) { bigFormMenuConfiguration.menuButtons = {}; }
 
-    bigFormMenuConfiguration.menuButtons["saveButton"] = new MenuButton(
-        'Save',
-        ()=> { console.log('####### Save button clicked'); },
-        'btn  btn-success btn-sm',
-        (createdSaveButton)=>{ 
-            menuSaveButton = createdSaveButton; 
-        });
+    bigFormMenuConfiguration.menuButtons["saveButton"] = {
+        caption: 'Save',
+        onClick: () => { console.log('####### Save button clicked'); },
+        cssClass: 'btn  btn-success btn-sm',
+        wireUp: (createdSaveButton) => {
+            menuSaveButton = createdSaveButton;
+        }
+    };
 
-    bigFormMenuConfiguration.menuButtons["deleteButton"] = new MenuButton(
-        'Delete',
-        ()=> { console.log('####### Delete button clicked'); },
-        'btn btn-danger btn-sm');
+    bigFormMenuConfiguration.menuButtons["deleteButton"] = {
+        caption: 'Delete',
+        onClick: () => { console.log('####### Delete button clicked'); },
+        cssClass: 'btn btn-danger btn-sm'
+    };
 
-    bigFormMenuConfiguration.menuButtons["cancelButton"] = new MenuButton(
-        'Cancel',
-        ()=> { console.log('####### Cancel button clicked'); },
-        'btn btn-link');
+    bigFormMenuConfiguration.menuButtons["cancelButton"] = {
+        caption: 'Cancel',
+        onClick: () => { console.log('####### Cancel button clicked'); },
+        cssClass: 'btn btn-link'
+    }
 }
-
--- bigformmenu/examples/html5form.bigformmenu.config.ts
+++ bigformmenu/examples/html5form.bigformmenu.config.ts
@@ -10,66 +10,52 @@ namespace BigFormMenu {
 
     export var bigFormMenuConfiguration: iBigFormMenuConfiguration;
 
-    class Html5ItemStateInfo implements  iItemStateInfo {
-        constructor(
-            public onChangeMenuItemsWithItemStateExist: (exist: boolean)=>void,
-            public hasActiveStateClass: string,
-            public stateFilterActiveClass: string,
-            public stateFilterButtonClass: string,
-            public wireUp: (domElement: HTMLElement, setActive: (active: boolean)=>void)=>void
-        ) {}
-    }
-
     bigFormMenuConfiguration.querySelector = "h1,h2,h3,h4,h5,h6,label";
 
     // if itemStateInfos is undefined, set it now
     if (!bigFormMenuConfiguration.itemStateInfos) { bigFormMenuConfiguration.itemStateInfos = {}; }
 
-    bigFormMenuConfiguration.itemStateInfos["html5required"] = new Html5ItemStateInfo(
-        null,
-        'bigformmenu-is-required', 
-        'bigformmenu-required-filter-is-active', 'bigformmenu-required-filter-button', 
-        (domElement: HTMLElement, setActive: (active: boolean)=>void)=> {
+    bigFormMenuConfiguration.itemStateInfos["html5required"] = {
+        onChangeMenuItemsWithItemStateExist: null,
+        hasActiveStateClass: 'bigformmenu-is-required',
+        stateFilterActiveClass: 'bigformmenu-required-filter-is-active',
+        stateFilterButtonClass: 'bigformmenu-required-filter-button',
+        wireUp: (domElement: HTMLElement, setActive: (active: boolean) => void) => {
             if (domElement.tagName.toLowerCase() !== 'label') { return; }
-
-// DOES NOT WORK FOR select, textarea, etc.
-
-
             let labelElement: HTMLLabelElement = domElement as HTMLLabelElement;
             let inputElementId = labelElement.htmlFor;
             let inputElement: HTMLInputElement = document.getElementById(inputElementId) as HTMLInputElement;
             setActive(inputElement.required);
-        });
+        }
+    };
 
-        bigFormMenuConfiguration.itemStateInfos["html5invalid"] = new Html5ItemStateInfo(
-            (exist: boolean)=>{ 
+    bigFormMenuConfiguration.itemStateInfos["html5invalid"] = {
+        onChangeMenuItemsWithItemStateExist: (exist: boolean)=> {
+            // Disable the save button if there are invalid input elements.
+            // Note that when this callback starts getting called, the buttons will not yet be in the DOM,
+            // so use the button returned in the wireUp method of the menuButtons object.
 
-                // Disable the save button if there are invalid input elements.
-                // Note that when this callback starts getting called, the buttons will not yet be in the DOM,
-                // so use the button returned in the wireUp method of the menuButtons object.
+            if (menuSaveButton) {
+                menuSaveButton.disabled = exist;
+            }
+        },
+        hasActiveStateClass: 'bigformmenu-is-invalid',
+        stateFilterActiveClass: 'bigformmenu-invalid-filter-is-active',
+        stateFilterButtonClass: 'bigformmenu-invalid-filter-button',
+        wireUp: (domElement: HTMLElement, setActive: (active: boolean) => void) => {
+            if (domElement.tagName.toLowerCase() !== 'label') { return; }
 
-                if (menuSaveButton) {
-                    menuSaveButton.disabled = exist;
-                }
-            },
-            'bigformmenu-is-invalid',
-            'bigformmenu-invalid-filter-is-active', 'bigformmenu-invalid-filter-button', 
-            (domElement: HTMLElement, setActive: (active: boolean)=>void)=> {
-                if (domElement.tagName.toLowerCase() !== 'label') { return; }
-    
-                let labelElement: HTMLLabelElement = domElement as HTMLLabelElement;
-                let inputElementId = labelElement.htmlFor;
-                let inputElement: HTMLInputElement = document.getElementById(inputElementId) as HTMLInputElement;
+            let labelElement: HTMLLabelElement = domElement as HTMLLabelElement;
+            let inputElementId = labelElement.htmlFor;
+            let inputElement: HTMLInputElement = document.getElementById(inputElementId) as HTMLInputElement;
 
+            setActive(!inputElement.validity.valid);
 
-                // DOES NOT WORK FOR select, textarea, etc.
-
+            inputElement.addEventListener("input", function () {
                 setActive(!inputElement.validity.valid);
-
-                inputElement.addEventListener("input", function(){
-                    setActive(!inputElement.validity.valid);
-                });
             });
-    }
+        }
+    };
+}
 
 
