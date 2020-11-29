3d54010e206ed399197885a0efc47ec870942767
mperdeck <black@hole.com>
2020-11-29 13:38:03 +1100

    Move to VS project, to get webcompiler to work

--- bigformmenu/bigformmenu.ts
+++ bigformmenu/bigformmenu.ts
@@ -1,7 +1,7 @@
 ///<reference path="bigformmenu.d.ts" />
 
 // "DOMContentLoaded" fires when the html has loaded, even if the stylesheets, javascript, etc are still being loaded.
-// "load" fires when the entire page has loaded, including stylesheets, etc.
+// "load" fires when the entire page has loaded, including stylesheets, etc. 
 
 document.addEventListener("DOMContentLoaded", function(){
     BigFormMenu.pageLoadedHandler();
